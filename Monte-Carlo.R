44a6113c4a6bac75d2a7392bca26f06ce3667fed
CRIWKINT <chris.oe@hotmail.com>
2021-02-24 12:46:51 +0100

    Opdatering af monte-carlo

--- Monte-Carlo.R
+++ Monte-Carlo.R
@@ -56,7 +56,7 @@ graph(plt())
 E_S_t <- function(n,t0,y0,delta_t,W_0=0,k,mu,sigma){
   z <- rep(NA,n) #Holder Euler-værdierne
   for (i in 1:n){
-    z[i]<-Euler(t0,y0,delta_t,W_0=0,k,mu,sigma)[k+1]
+    z[i]<-Euler(1/1000,1000)[k+1]
   }
   a<- mean(z)
   return(a)
@@ -64,8 +64,50 @@ E_S_t <- function(n,t0,y0,delta_t,W_0=0,k,mu,sigma){
 
 monte<-E_S_t(10000,0,10,1/1000,0,1000,0.07,0.2)
 #T=10001
-mu_log <- (0.07-0.5*0.2^2)*(1001/1000)+log(10) 
+mu_log <- (0.07-0.5*0.2^2)+log(10) 
 exp(mu_log+(0.2^2)/2) #Den teoretiske værdi
 error<- monte-exp(mu_log+(1001/1000)*(0.2^2)/2) #Error
 
+zy<-rep(NA,100000)
+for (i in 1:100000){
+  zy[i]<-rlnorm(1,mu_log,sqrt(0.2^2*0.5*(1001/1000)))
+}
+mean(zy)
 
+
+
+#Opg h)
+J<-1000000
+L<- 5000
+muhat<-rep(NA,J)
+muhatlog<-rep(NA,J)
+sigmahat<-rep(NA,J)
+for (i in 1:J){
+  a<-Euler(1/500,L, y0=10)
+  logp<-(log(a)-log(lag(a)))[2:L+1]
+  muhat[i]<-(mean(logp))*500+0.5*var(logp)*500
+  muhatlog[i]<-mean(logp)
+  sigmahat[i]<- var(logp)
+  print(i)
+}
+mean(muhat)
+var(muhatlog)
+var(muhat)
+median(muhat)
+mean(sigmahat*500)
+var(sigmahat*500)
+cor(muhat,sigmahat*500)
+
+
+
+
+0.04/(L*1/(500)) + 0.04^2/(2*L)
+var(muhat)
+sd(muhat)
+
+0.04^2*2/L
+var(sigmahat*500)
+
+
+0.04^2/(L)
+cov(sigmahat*500,muhat)
